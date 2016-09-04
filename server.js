324c6f38a0e5e0b2154b6925797e129f65c73850
Leonel Badi <lbadi@itba.edu.ar>
2016-09-04 16:12:08 -0300

    cors

--- server.js
+++ server.js
@@ -4,9 +4,17 @@ var bodyParser  = require('body-parser');
 var app = express();
 var port = process.env.PORT || 8080;
 var mailing = require('./mailing');
+var cors_config = require('./cors_config');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+//Enable cors for every endpoint.
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", cors_config.origin);
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.use('/mailing',mailing);
 
